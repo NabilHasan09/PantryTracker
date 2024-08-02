2f3d02e8e48e9cd294e4a43e6d65ca2d838270f0
NabilHasan09 <nabilhasan123456@gmail.com>
2024-08-02 19:26:02 -0400

    implement add and delete function

--- src/app/firebase.js
+++ src/app/firebase.js
@@ -1,21 +1,27 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
+import {getFirestore} from "firebase/firestore"
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// Import the functions you need from the SDKs you need
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBJx1djl_aiwpTepmuHT2MBJfCyoqGGwtM",
-  authDomain: "pantryai-a6ea5.firebaseapp.com",
-  projectId: "pantryai-a6ea5",
-  storageBucket: "pantryai-a6ea5.appspot.com",
-  messagingSenderId: "648995020679",
-  appId: "1:648995020679:web:8d87a2c877790e82e5bd22",
-  measurementId: "G-PQR2CTNV0T"
+  apiKey: "AIzaSyCEUzkco8H_9d-k3YjQd03SQ4EY2sj69q4",
+  authDomain: "pantry-app-v1.firebaseapp.com",
+  projectId: "pantry-app-v1",
+  storageBucket: "pantry-app-v1.appspot.com",
+  messagingSenderId: "808653746294",
+  appId: "1:808653746294:web:1a8b48b5634226906daf57"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+const firestore = getFirestore(app)
+
+export {app, firestore}
--- src/app/page.js
+++ src/app/page.js
@@ -1,4 +1,9 @@
-import { Box, Stack, Typography } from "@mui/material";
+'use client'
+import { Box, Stack, Typography, Button, Modal, TextField} from "@mui/material";
+import { firestore } from "./firebase";
+import { useEffect, useState } from "react";
+import { deleteDoc, query } from "firebase/firestore";
+import { collection, getDocs, setDoc, doc } from "firebase/firestore";
 
 const commonStyles = {
   bgcolor: 'background.paper',
@@ -9,63 +14,157 @@ const commonStyles = {
   height: '5rem',
 };
 
-const items = ['apple', 'cherry', 'donut', 'orange', 'apple', 'cherry', 'donut', 'orange']
+const style = {
+  position: 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  width: 400,
+  bgcolor: 'white',
+  border: '2px solid #000',
+  boxShadow: 24,
+  p: 4,
+};
+
 
 export default function Home() {
-  // We'll add our component logic here
-  return ( <Box 
-    width="100vw" 
-    height="100vh"
-    display = 'flex'
-    justifyContent={'center'}
-    alignItems={'center'}
-    flexDirection={'column'}>
-    <Box border={'2px solid #333'}>  
-      <Box 
-        width={'800px'}
-        height={'100px'}
-        bgcolor={'#FFCCCB'}
-        display = 'flex'
-        justifyContent={'center'}
-        alignItems={'center'}
+  // useState hook is used to initialize a state variable called data
+  //This variable will store the data retrieved from an API endpoint.
+  const [pantry, setPantry ] = useState([])
+  
+  const [open, setOpen] = useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
 
-      >
-        <Typography
-          variant="h2"
-          color={'#333'}
-          textAlign={'center'}
-          
-          
-        >
-          Pantry Items
-        </Typography>
-      </Box>
-      <Stack width={'800px'} height={'400px'} spacing={2} overflow={'scroll'}>
-        {items.map((i) => (
-          <Box
-            key={i}
-            width={'100%'}
-            height={'100px'}
-            display={'flex'}
-            justifyContent={'center'}
-            padding={'10px'}
-            alignItems={'center'}
-            bgcolor={'#f0f0f0'}
-          >
-            <Typography 
-              variant="h5"
-              color={'#333'}
-              textAlign={'center'}
-            >
-              {
-                i.charAt(0).toUpperCase() + i.slice(1)
-              }
-            </Typography>
-          </Box>
-        ))} 
-      </Stack>
-    </Box>
-  </Box>
+  const [itemName, setItemName] = useState('');
+  // The useEffect hook is used to request data from the API endpoint 
+  //once the component initially renders
+
+  const updatePantry = async () => {
+    const snapshot = query(collection(firestore, 'pantry'))
+    // gets documents/pantry items 
+    const docs = await getDocs(snapshot)
+    //converts to list 
+    const pantryList = []
+    docs.forEach((doc) => {
+      pantryList.push(doc.id)
+    })
+    setPantry(pantryList)
+  }
+  useEffect (() => {
+    updatePantry()
+  }, [])
+  // add function 
+  const addItem = async (item) => {
+    const docRef = doc(collection(firestore,'pantry'),item)
+    await setDoc(docRef,{})
+    await updatePantry()
+  }
+  
+  // Delete function
+  const deleteItem = async (item) => {
+    const docRef = doc(collection(firestore,'pantry'),item)
+    await deleteDoc(docRef)
+    await updatePantry()
+  }
+
+  return ( 
     
+    <Box 
+      width="100vw" 
+      height="100vh"
+      display = 'flex'
+      justifyContent={'center'}
+      alignItems={'center'}
+      flexDirection={'column'}
+      gap={2}
+    >
+      <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box sx= {style}>
+          <Typography id ="modal-modal-title" variant="h6" component={'h2'}>
+            Add Item
+          </Typography>
+          <Stack width={'100%'}direction = {'row'} spacing ={2}>
+            <TextField 
+              id="outlined-basic" 
+              label="Item" 
+              variant="outlined" 
+              fullWidth
+              value={itemName}
+              onChange={(e)=>setItemName(e.target.value)}
+            />
+            <Button 
+              variant="outlined" 
+              onClick={() => {
+                addItem(itemName)
+                setItemName('')
+                handleClose();
+              }}
+            >
+              Add 
+            </Button>
+          </Stack>
+        </Box>
+      </Modal>
+      <Button variant = "contained" onClick={handleOpen}>Add Item</Button>
+
+      <Box border={'2px solid #333'}>  
+        
+        <Box 
+          width={'800px'}
+          height={'100px'}
+          bgcolor={'#FFCCCB'}
+          display = 'flex'
+          justifyContent={'center'}
+          alignItems={'center'}
+
+        >
+          <Typography
+            variant="h2"
+            color={'#333'}
+            textAlign={'center'} 
+          >
+            Pantry Items
+          </Typography>
+
+        </Box>
+        
+        <Stack width={'800px'} height={'400px'} spacing={2} overflow={'auto'}>
+          {pantry.map((i) => (
+            <Box
+              key={i}
+              width={'100%'}
+              height={'100px'}
+              display={'flex'}
+              justifyContent={'space-between'}
+              alignItems={'center'}
+              bgcolor={'#f0f0f0'}
+              paddingX={5}
+            >
+              <Typography 
+                variant="h5"
+                color={'#333'}
+                textAlign={'center'}
+              >
+                {
+                  i.charAt(0).toUpperCase() + i.slice(1)
+                }
+              </Typography>
+              <Button variant="" onClick={() => {
+                deleteItem(i)
+                
+              }}>
+                Delete
+              </Button>
+            </Box>
+          ))} 
+        </Stack>
+      </Box>
+    </Box>    
   )
 }
