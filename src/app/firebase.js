3cd0fdc8ac41eb24d583132598c0904dfbd3ebda
NabilHasan09 <nabilhasan123456@gmail.com>
2024-08-04 14:56:34 -0400

    add environment variables to hide sensitive info

--- src/app/firebase.js
+++ src/app/firebase.js
@@ -1,6 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import {getFirestore} from "firebase/firestore"
+import { getFirestore} from "firebase/firestore"
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -12,12 +12,12 @@ import {getFirestore} from "firebase/firestore"
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCEUzkco8H_9d-k3YjQd03SQ4EY2sj69q4",
-  authDomain: "pantry-app-v1.firebaseapp.com",
-  projectId: "pantry-app-v1",
-  storageBucket: "pantry-app-v1.appspot.com",
-  messagingSenderId: "808653746294",
-  appId: "1:808653746294:web:1a8b48b5634226906daf57"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
